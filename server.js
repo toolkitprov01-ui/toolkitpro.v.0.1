27bc17419814c49e9c97c088097543f7cf965358
toolkitprov01-ui <toolkitpro.v.0.1@gmail.com>
2026-09-18 07:21:33 +0600

    Fix Express 5 catch-all route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
